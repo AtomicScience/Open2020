9678dd25d575d40449d4460bdc8ba15f0d06b12f
MrShelDie <nick-ivchenko2@mail.ru>
2020-10-24 23:11:34 +0300

    Обновил проект до последней версии

--- temporary_front/js/button_disabler.js
+++
@@ -1,8 +0,0 @@
-$('#one, #two').blur(function() {
-    let btn = $('#enter')
-    if($('#one').val() == '' || $('#two').val() == '') {
-        btn.prop('disabled', true);
-    } else {
-        btn.prop('disabled', false);
-    }
- });
